ba3c319f5da9d8a2807747359d414b79fed4fc77
rareprob <arunsaini065@gmail.com>
2021-10-13 14:40:58 +0530

    question

--- src/comp/FeedBackItem.jsx
+++ src/comp/FeedBackItem.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
+import React,{useState} from 'react'
 import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowCircleDown } from '@fortawesome/free-solid-svg-icons'
 const FeedBackItem = ({ item }) => {
-    return (
-        <Container>
-            <img src={item.image} alt={item.image} width={50} height={50} />
+    const [question, setquestion] = useState(false)
+    return (<>
+        <Container onClick={()=>setquestion(!question)}>
+            <img src={item.image} alt={item.image} width={50} height={50} style={{}}/>
             <h2 style={{ flex: 1, marginLeft: 50 }}>{item.name}</h2>
             <DownLink >
                 <FontAwesomeIcon icon={faArrowCircleDown} />
             </DownLink>
         </Container>
+        {question&&    <Questions >
+          <h3>{item.q1}</h3>
+          <h3>{item.q2}</h3>
+          <h3>{item.q3}</h3>
+          <h3>{item.q4}</h3>
+          <h3>{item.q5}</h3>
+          <h3>{item.q6}</h3>
+          <h3>{item.q7}</h3>
+          <h3>{item.q8}</h3>
+          <h3>{item.q9}</h3>
+          <h3>{item.q10}</h3>
+          <h3>{item.q11}</h3>
+          <h3>{item.q12}</h3>
+          <h3>{item.q13}</h3>
+          <h3>{item.q14}</h3>
+          <h3>{item.q15}</h3>
+       </Questions>
+            }
+        </>
     )
 }
 export default FeedBackItem
@@ -28,4 +48,11 @@ const DownLink = styled.div`
 width: 30px;
 cursor: pointer;
 color: white;
+`
+
+const Questions=styled.div`
+display: flex;
+flex-direction: column;
+width: 70%;
+margin: auto;
 `
---
+++ src/comp/Ques.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Ques = () => {
+    return (
+        <div>
+            <h1>Hi</h1>
+        </div>
+    )
+}
+
+export default Ques
