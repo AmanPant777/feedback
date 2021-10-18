50352e908725ff49e89a7cd1e0e7317ffd2f0bed
rareprob <arunsaini065@gmail.com>
2021-10-18 16:23:31 +0530

    add

--- src/comp/Question/Audio.jsx
+++ src/comp/Question/Audio.jsx
@@ -2,30 +2,67 @@ import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import styled from 'styled-components'
+import {useHistory} from 'react-router-dom'
 const data=[
     {
-       question:'Q1.How do I change the theme of the Player while listening to any song?'
+       question:'Q1.How do I change the theme of the Player while listening to any song?',
+       answer:{
+           line1:'Yu can easily change the theme by following these steps.',
+           line2:'1.Click on the Theme icon',
+           line3:'2.Click on the use button',
+           image1:'/images/audio/Aq1P1.png',
+           image2:'/images/audio/Aq1P2.png',
+           answer2:{
+               line1:'1.Go to Hamburger and click on Theme button',
+               line2:'2.Click on Player Theme banner.',
+               image1:'/images/audio/Aq1P3.png',
+               image2:'/images/audio/Aq1P4.png',
+           }
+       }
     },
     {
-        question:'Q2.Is it possible to stop the playlist automatically after some time?'
+        question:'Q2.Is it possible to stop the playlist automatically after some time?',
+        answer:{
+            line1:'Yu can set the sleep timer and automatically yur playlist will be stoped.',
+            line2:'Click on Sleep icon',
+            image1:'/images/audio/Aq2P1.png'
+        }
     },
     {
-        question:'Q3.How can I change the sequence of songs in the playlist?'
+        question:'Q3.How can I change the sequence of songs in the playlist?',
+        answer:{
+            line1:'Yu can tap here and change the sequence accordingly',
+            line2:'Drag the song and place where yu want.',
+            image1:'/images/audio/Aq3P1.png'
+        }
     },
     {
-        question:'Q4.Why am I not able to get different themes on the player screen?'
+        question:'Q4.Why am I not able to get different themes on the player screen?',
+        answer:{
+            line1:'Few Themes are available but for lot more themes yu can take a premium membership and enjoy he various themes available.'
+        }
     },
     {
-        question:'Q5.Is it possible to share a song directly from the Player\'s screen?'
+        question:'Q5.Is it possible to share a song directly from the Player\'s screen?',
+        answer:{
+            line1:'Yes yu can share it easily with yur friends',
+            line2:'1.Click on three Dot',
+            line3:'2.Click Share icon',
+            image1:'/images/audio/Aq5P1.png',
+            image2:'/images/audio/Aq5P2.png'
+        }
     },
 ]
 
 const Audio = () => {
+    const history=useHistory()
     return (
         <div>
             {data.map((item)=>{
                 return (
-                  <Container>
+                  <Container onClick={()=>history.push(
+                      {pathname:'/answer',
+                      state:{question:item.question,answer:item.answer}})}>
                       <Questions>{item.question}</Questions>
                       <FontAwesomeIcon icon={faArrowRight}/>
                   </Container>
--- src/comp/Question/Download.jsx
+++ src/comp/Question/Download.jsx
@@ -1,31 +1,60 @@
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
 import styled from 'styled-components'
 const data=[
     {
-       question:'Q1.How to download videos/photos directly from Instagram without login?'
+       question:'Q1.How to download videos/photos directly from Instagram without login?',
+       answer:{
+           line1:'Copy the URL of the photo/video yu want to download.',
+           image1:'/images/download/Dq1P1.png',
+       }
     },
     {
-        question:'Q2.Why downloading speed is low sometimes?'
+        question:'Q2.Why downloading speed is low sometimes?',
+        answer:{
+            line1:'Yu may have to check the mobile data connection speed and if the connection is strong. ',
+            line2:'Then yu may reboot yur phone and start downloading again.'
+        }
     },
     {
-        question:'Q3.How can I change the storage site of my downloaded videos/photos?'
+        question:'Q3.How can I create playlist of my downloaded videos/photos.',
+        answer:{
+            line1:'The Steps are',
+            line2:'1.Click on Download.',
+            line3:'Click on three dots.',
+            line4:'Click on Add Playlist.',
+            image1:'/images/download/Dq3P1.png',
+            image2:'/images/download/Dq3P2.png',
+            image3:'/images/download/Dq3P3.png',
+        }
     },
     {
-        question:'Q4.How can I pause or cancel the downloading?'
+        question:'Q4.How can I pause or cancel the downloading?',
+        answer:{
+            line1:'Click on progress tab and yu can pause or cancel the download',
+            image1:'/images/download/Dq4p1.png'
+        }
     },
     {
-        question:'Q5.Sometimes, why is it asking for a login for downloading any videos/photos?'
+        question:'Q5.Sometimes, why is it asking for a login for downloading any videos/photos?',
+        answer:{
+            line1:'If the video/photo yu want to download belongs to a private Instagram account.Then only in that case yu have to login to yur Instagram account.'
+        }
     },
 ]
 
 const Download = () => {
+    const history=useHistory()
     return (
         <div>
             {data.map((item)=>{
                 return (
-                  <Container>
+                  <Container onClick={()=>history.push({
+                      pathname:'/answer',
+                      state:{question:item.question,answer:item.answer}
+                  })}>
                       <Questions>{item.question}</Questions>
                       <FontAwesomeIcon icon={faArrowRight}/>
                   </Container>
